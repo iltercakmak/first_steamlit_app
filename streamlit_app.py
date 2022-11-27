a431430df710cd9917f1ed063d3f80cf66f04fd2
iltercakmak <112896691+iltercakmak@users.noreply.github.com>
2022-11-27 12:28:15 -0500

    Update streamlit_app.py
    Changed the Header Text
    Added an emoji to the second line

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title("Trying Streamlit")
 
-streamlit.header("This is a header")
+streamlit.header("This is a header V2")
 streamlit.text("This is the first line below the header.")
-streamlit.text("And this is the second line. Just trying things out.")
+streamlit.text("🐔And this is the second line. Just trying things out.")
