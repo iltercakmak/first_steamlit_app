eb46932302b2649ac88ac67d3149198ea01314cd
iltercakmak <112896691+iltercakmak@users.noreply.github.com>
2022-11-27 12:28:49 -0500

    Update streamlit_app.py
    Created a new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.title("Trying Streamlit")
 streamlit.header("This is a header V2")
 streamlit.text("This is the first line below the header.")
 streamlit.text("🐔And this is the second line. Just trying things out.")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
